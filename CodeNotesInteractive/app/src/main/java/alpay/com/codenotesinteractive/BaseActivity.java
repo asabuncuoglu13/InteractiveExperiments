3801b7de2ab8fa91306eeb773cc5a0ad3f0fb1f8
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2018-01-22 18:16:36 +0300

    TapTargetViewAdded

--- CodeNotesInteractive/app/src/main/java/alpay/com/codenotesinteractive/BaseActivity.java
+++ CodeNotesInteractive/app/src/main/java/alpay/com/codenotesinteractive/BaseActivity.java
@@ -7,6 +7,9 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.getkeepsafe.taptargetview.TapTarget;
+import com.getkeepsafe.taptargetview.TapTargetView;
+
 import alpay.com.codenotesinteractive.chat.ChatActivity;
 import alpay.com.codenotesinteractive.compiler.CompilerActivity;
 import alpay.com.codenotesinteractive.simulation.SimulationActivity;
@@ -18,6 +21,25 @@ public class BaseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        TapTargetView.showFor(this,                 // `this` is an Activity
+                TapTarget.forView(findViewById(R.id.bottom_navigation), getString(R.string.bottom_target_title), getString(R.string.bottom_target_detail))
+                        // All options below are optional
+                        .outerCircleColor(R.color.colorPrimaryDark)      // Specify a color for the outer circle
+                        .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
+                        .drawShadow(true)                   // Whether to draw a drop shadow or not
+                        .cancelable(true)                  // Whether tapping outside the outer circle dismisses the view
+                        .tintTarget(true)                   // Whether to tint the target view's color
+                        .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
+                        .targetRadius(60),                  // Specify the target radius (in dp)
+                new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
+                    @Override
+                    public void onTargetClick(TapTargetView view) {
+                        super.onTargetClick(view);      // This call is optional
+                        //
+                    }
+                });
+
         BottomNavigationView bnv = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
