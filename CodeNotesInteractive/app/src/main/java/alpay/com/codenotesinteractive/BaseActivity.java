34e2a880dfdd05224b8566224fe5c96bcf356648
asabuncuoglu13 <asabuncuoglu13@ku.edu.tr>
2018-01-22 22:24:27 +0300

    ActionBarMenuAdded

--- CodeNotesInteractive/app/src/main/java/alpay/com/codenotesinteractive/BaseActivity.java
+++ CodeNotesInteractive/app/src/main/java/alpay/com/codenotesinteractive/BaseActivity.java
@@ -6,6 +6,8 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.getkeepsafe.taptargetview.TapTarget;
@@ -28,6 +30,7 @@ public class BaseActivity extends AppCompatActivity {
     ViewPager viewPager;
     BottomNavigationView bottomNavigation;
     MenuItem prevMenuItem;
+    static boolean showTapTarget = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +40,28 @@ public class BaseActivity extends AppCompatActivity {
         viewPager = findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 
-        TapTargetView.showFor(this,                 // `this` is an Activity
-                TapTarget.forView(findViewById(R.id.bottom_navigation), getString(R.string.bottom_target_title), getString(R.string.bottom_target_detail))
-                        // All options below are optional
-                        .outerCircleColor(R.color.colorPrimaryDark)      // Specify a color for the outer circle
-                        .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
-                        .drawShadow(true)                   // Whether to draw a drop shadow or not
-                        .cancelable(true)                  // Whether tapping outside the outer circle dismisses the view
-                        .tintTarget(true)                   // Whether to tint the target view's color
-                        .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
-                        .targetRadius(60),                  // Specify the target radius (in dp)
-                new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
-                    @Override
-                    public void onTargetClick(TapTargetView view) {
-                        super.onTargetClick(view);      // This call is optional
-                        //
-                    }
-                });
+        if(showTapTarget)
+        {
+            TapTargetView.showFor(this,                 // `this` is an Activity
+                    TapTarget.forView(findViewById(R.id.bottom_navigation), getString(R.string.bottom_target_title), getString(R.string.bottom_target_detail))
+                            // All options below are optional
+                            .outerCircleColor(R.color.colorPrimaryDark)      // Specify a color for the outer circle
+                            .outerCircleAlpha(0.96f)            // Specify the alpha amount for the outer circle
+                            .drawShadow(true)                   // Whether to draw a drop shadow or not
+                            .cancelable(true)                  // Whether tapping outside the outer circle dismisses the view
+                            .tintTarget(true)                   // Whether to tint the target view's color
+                            .transparentTarget(false)           // Specify whether the target is transparent (displays the content underneath)
+                            .targetRadius(60),                  // Specify the target radius (in dp)
+                    new TapTargetView.Listener() {          // The listener can listen for regular clicks, long clicks or cancels
+                        @Override
+                        public void onTargetClick(TapTargetView view) {
+                            super.onTargetClick(view);      // This call is optional
+                            //
+                        }
+                    });
+        }
+
+        showTapTarget = false;
 
 
         bottomNavigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
@@ -105,6 +113,27 @@ public class BaseActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_refresh was selected
+            case R.id.action_howto:
+                Intent intent = new Intent(this, HowToActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
 
     private void setupViewPager(ViewPager viewPager)
     {
