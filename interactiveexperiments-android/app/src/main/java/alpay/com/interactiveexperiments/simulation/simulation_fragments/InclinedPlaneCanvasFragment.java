fb22d254335ecb4e2ad1edbbf16eefe136da6579
Alpay Sabuncuoglu <asabuncuoglu13@ku.edu.tr>
2018-04-19 17:13:37 +0300

    SensorSimulationUpdate

--- interactiveexperiments-android/app/src/main/java/alpay/com/interactiveexperiments/simulation/simulation_fragments/InclinedPlaneCanvasFragment.java
+++ interactiveexperiments-android/app/src/main/java/alpay/com/interactiveexperiments/simulation/simulation_fragments/InclinedPlaneCanvasFragment.java
@@ -1,29 +1,61 @@
 package alpay.com.interactiveexperiments.simulation.simulation_fragments;
 
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import alpay.com.interactiveexperiments.R;
+import alpay.com.interactiveexperiments.util.OnSwipeTouchListener;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
+import static android.content.Context.POWER_SERVICE;
 import static android.content.Context.SENSOR_SERVICE;
+import static android.content.Context.WINDOW_SERVICE;
 
-public class InclinedPlaneCanvasFragment extends Fragment implements View.OnClickListener{
+public class InclinedPlaneCanvasFragment extends Fragment {
 
-    public View view;
-    SensorManager mSensorManager;
-    final float[] mValuesMagnet      = new float[3];
-    final float[] mValuesAccel       = new float[3];
+    final float[] mValuesMagnet = new float[3];
+    final float[] mValuesAccel = new float[3];
     final float[] mValuesOrientation = new float[3];
-    final float[] mRotationMatrix    = new float[9];
-    TextView sensor_text;
+    final float[] mRotationMatrix = new float[9];
+
+    View view;
+
+    Drawable[] backgroundDrawables = new Drawable[3];
+    final float[] frictionCoefficients = {2.5f, 0.5f, 6.0f};
+    int currentSurfaceIndex = 0;
+
+    private SimulationView mSimulationView;
+    private SensorManager mSensorManager;
+    private PowerManager mPowerManager;
+    private WindowManager mWindowManager;
+    private Display mDisplay;
+    private PowerManager.WakeLock mWakeLock;
+
 
     public InclinedPlaneCanvasFragment() {
 
@@ -32,56 +64,310 @@ public class InclinedPlaneCanvasFragment extends Fragment implements View.OnClic
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_inclined_plane_canvas, container, false);
-        SensorManager sensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
-
-        view.findViewById(R.id.sensor_button).setOnClickListener(this);
-        sensor_text = (TextView) view.findViewById(R.id.sensor_text);
-        final SensorEventListener mEventListener = new SensorEventListener() {
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            }
-
-            public void onSensorChanged(SensorEvent event) {
-                // Handle the events for which we registered
-                switch (event.sensor.getType()) {
-                    case Sensor.TYPE_ACCELEROMETER:
-                        System.arraycopy(event.values, 0, mValuesAccel, 0, 3);
-                        break;
-                    case Sensor.TYPE_MAGNETIC_FIELD:
-                        System.arraycopy(event.values, 0, mValuesMagnet, 0, 3);
-                        break;
-                    default:
-                        break;
-                }
-            };
-        };
-
-        // You have set the event listener up, now just need to register this with the
-        // sensor manager along with the sensor wanted.
-        setListeners(sensorManager, mEventListener);
-        return view;
+        mSensorManager = (SensorManager) getActivity().getSystemService(SENSOR_SERVICE);
+        mPowerManager = (PowerManager) getActivity().getSystemService(POWER_SERVICE);
+        mWindowManager = (WindowManager) getActivity().getSystemService(WINDOW_SERVICE);
+        mDisplay = mWindowManager.getDefaultDisplay();
+        mWakeLock = mPowerManager.newWakeLock(PowerManager.SCREEN_BRIGHT_WAKE_LOCK, getClass()
+                .getName());
+        mSimulationView = new SimulationView(getActivity());
+        mSimulationView.setBackgroundResource(R.drawable.wood);
+        initializeBackgroundDrawablesArray();
+        setSwipeAction();
+        return mSimulationView;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mWakeLock.acquire();
+        mSimulationView.startSimulation();
     }
 
-    // Register the event listener and sensor type.
-    public void setListeners(SensorManager sensorManager, SensorEventListener mEventListener)
-    {
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSimulationView.stopSimulation();
+        mWakeLock.release();
+    }
+
+    private void initializeBackgroundDrawablesArray() {
+        backgroundDrawables[0] = getResources().getDrawable(R.drawable.wood);
+        backgroundDrawables[1] = getResources().getDrawable(R.drawable.ice);
+        backgroundDrawables[2] = getResources().getDrawable(R.drawable.grass);
+    }
+
+    private void setSwipeAction() {
+        mSimulationView.setOnTouchListener(new OnSwipeTouchListener(this.getActivity()) {
+            public void onSwipeTop() {
+            }
+
+            public void onSwipeRight() {
+                currentSurfaceIndex++;
+                mSimulationView.setBackground(backgroundDrawables[currentSurfaceIndex % backgroundDrawables.length]);
+            }
+
+            public void onSwipeLeft() {
+                currentSurfaceIndex--;
+                mSimulationView.setBackground(backgroundDrawables[currentSurfaceIndex % backgroundDrawables.length]);
+            }
+
+            public void onSwipeBottom() {
+            }
+
+        });
+    }
+
+    private void setListeners(SensorManager sensorManager, SensorEventListener mEventListener) {
         sensorManager.registerListener(mEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
                 SensorManager.SENSOR_DELAY_NORMAL);
         sensorManager.registerListener(mEventListener, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
                 SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override
-    public void onClick(View v) {
-        int i = v.getId();
-        if(i == R.id.sensor_button)
-        {
-            SensorManager.getRotationMatrix(mRotationMatrix, null, mValuesAccel, mValuesMagnet);
-            SensorManager.getOrientation(mRotationMatrix, mValuesOrientation);
-            final CharSequence test;
-            test = "results: " + mValuesOrientation[0]*90 +" "+mValuesOrientation[1] * 90 + " "+ mValuesOrientation[2] *90;
-            sensor_text.setText(test);
+    class SimulationView extends FrameLayout implements SensorEventListener {
+        private static final float sBallDiameter = 0.004f;
+        private static final float sBallDiameter2 = sBallDiameter * sBallDiameter;
+
+        private final int mDstWidth;
+        private final int mDstHeight;
+
+        private Sensor mAccelerometer;
+        private long mLastT;
+
+        private float mXDpi;
+        private float mYDpi;
+        private float mMetersToPixelsX;
+        private float mMetersToPixelsY;
+        private float mXOrigin;
+        private float mYOrigin;
+        private float mSensorX;
+        private float mSensorY;
+        private float mHorizontalBound;
+        private float mVerticalBound;
+        private final ParticleSystem mParticleSystem;
+
+        class Particle extends View {
+            private float mPosX = (float) Math.random();
+            private float mPosY = (float) Math.random();
+            private float mVelX;
+            private float mVelY;
+
+            public Particle(Context context) {
+                super(context);
+            }
+
+            public Particle(Context context, AttributeSet attrs) {
+                super(context, attrs);
+            }
+
+            public Particle(Context context, AttributeSet attrs, int defStyleAttr) {
+                super(context, attrs, defStyleAttr);
+            }
+
+            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+            public Particle(Context context, AttributeSet attrs, int defStyleAttr,
+                            int defStyleRes) {
+                super(context, attrs, defStyleAttr, defStyleRes);
+            }
+
+            public void computePhysics(float sx, float sy, float dT) {
+
+                float friction = frictionCoefficients[currentSurfaceIndex];
+
+                final float ax = -sx / (5 * friction);
+                final float ay = -sy / (5 * friction);
+
+                mPosX += mVelX * dT + ax * dT * dT / 2;
+                mPosY += mVelY * dT + ay * dT * dT / 2;
+
+                mVelX += ax * dT;
+                mVelY += ay * dT;
+            }
+
+            public void resolveCollisionWithBounds() {
+                final float xmax = mHorizontalBound;
+                final float ymax = mVerticalBound;
+                final float x = mPosX;
+                final float y = mPosY;
+                if (x > xmax) {
+                    mPosX = xmax;
+                    mVelX = 0;
+                } else if (x < -xmax) {
+                    mPosX = -xmax;
+                    mVelX = 0;
+                }
+                if (y > ymax) {
+                    mPosY = ymax;
+                    mVelY = 0;
+                } else if (y < -ymax) {
+                    mPosY = -ymax;
+                    mVelY = 0;
+                }
+            }
+        }
+
+
+        class ParticleSystem {
+            static final int NUM_PARTICLES = 5;
+            private Particle mBalls[] = new Particle[NUM_PARTICLES];
+
+            ParticleSystem() {
+
+                for (int i = 0; i < mBalls.length; i++) {
+                    mBalls[i] = new Particle(getContext());
+                    mBalls[i].setBackgroundResource(R.drawable.ball);
+                    mBalls[i].setLayerType(LAYER_TYPE_HARDWARE, null);
+                    addView(mBalls[i], new ViewGroup.LayoutParams(mDstWidth, mDstHeight));
+                }
+            }
+
+            private void updatePositions(float sx, float sy, long timestamp) {
+                final long t = timestamp;
+                if (mLastT != 0) {
+                    final float dT = (float) (t - mLastT) / 1000.f;
+                    final int count = mBalls.length;
+                    for (int i = 0; i < count; i++) {
+                        Particle ball = mBalls[i];
+                        ball.computePhysics(sx, sy, dT);
+                    }
+                }
+                mLastT = t;
+            }
+
+            public void update(float sx, float sy, long now) {
+                updatePositions(sx, sy, now);
+                final int NUM_MAX_ITERATIONS = 10;
+
+                boolean more = true;
+                final int count = mBalls.length;
+                for (int k = 0; k < NUM_MAX_ITERATIONS && more; k++) {
+                    more = false;
+                    for (int i = 0; i < count; i++) {
+                        Particle curr = mBalls[i];
+                        for (int j = i + 1; j < count; j++) {
+                            Particle ball = mBalls[j];
+                            float dx = ball.mPosX - curr.mPosX;
+                            float dy = ball.mPosY - curr.mPosY;
+                            float dd = dx * dx + dy * dy;
+                            if (dd <= sBallDiameter2) {
+                                dx += ((float) Math.random() - 0.5f) * 0.0001f;
+                                dy += ((float) Math.random() - 0.5f) * 0.0001f;
+                                dd = dx * dx + dy * dy;
+                                final float d = (float) Math.sqrt(dd);
+                                final float c = (0.5f * (sBallDiameter - d)) / d;
+                                final float effectX = dx * c;
+                                final float effectY = dy * c;
+                                curr.mPosX -= effectX;
+                                curr.mPosY -= effectY;
+                                ball.mPosX += effectX;
+                                ball.mPosY += effectY;
+                                more = true;
+                            }
+                        }
+                        curr.resolveCollisionWithBounds();
+                    }
+                }
+            }
+
+            public int getParticleCount() {
+                return mBalls.length;
+            }
+
+            public float getPosX(int i) {
+                return mBalls[i].mPosX;
+            }
+
+            public float getPosY(int i) {
+                return mBalls[i].mPosY;
+            }
+        }
+
+        public void startSimulation() {
+            mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_GAME);
+        }
+
+        public void stopSimulation() {
+            mSensorManager.unregisterListener(this);
+        }
+
+        public SimulationView(Context context) {
+            super(context);
+            mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+            DisplayMetrics metrics = new DisplayMetrics();
+            getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+            mXDpi = metrics.xdpi;
+            mYDpi = metrics.ydpi;
+            mMetersToPixelsX = mXDpi / 0.0254f;
+            mMetersToPixelsY = mYDpi / 0.0254f;
+
+            // rescale the ball so it's about 0.5 cm on screen
+            mDstWidth = (int) (sBallDiameter * mMetersToPixelsX + 0.5f);
+            mDstHeight = (int) (sBallDiameter * mMetersToPixelsY + 0.5f);
+            mParticleSystem = new ParticleSystem();
+
+            BitmapFactory.Options opts = new BitmapFactory.Options();
+            opts.inDither = true;
+            opts.inPreferredConfig = Bitmap.Config.RGB_565;
+        }
+
+        @Override
+        protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+            mXOrigin = (w - mDstWidth) * 0.5f;
+            mYOrigin = (h - mDstHeight) * 0.5f;
+            mHorizontalBound = ((w / mMetersToPixelsX - sBallDiameter) * 0.5f);
+            mVerticalBound = ((h / mMetersToPixelsY - sBallDiameter) * 0.5f);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() != Sensor.TYPE_ACCELEROMETER)
+                return;
+            switch (mDisplay.getRotation()) {
+                case Surface.ROTATION_0:
+                    mSensorX = event.values[0];
+                    mSensorY = event.values[1];
+                    break;
+                case Surface.ROTATION_90:
+                    mSensorX = -event.values[1];
+                    mSensorY = event.values[0];
+                    break;
+                case Surface.ROTATION_180:
+                    mSensorX = -event.values[0];
+                    mSensorY = -event.values[1];
+                    break;
+                case Surface.ROTATION_270:
+                    mSensorX = event.values[1];
+                    mSensorY = -event.values[0];
+                    break;
+            }
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            final ParticleSystem particleSystem = mParticleSystem;
+            final long now = System.currentTimeMillis();
+            final float sx = mSensorX;
+            final float sy = mSensorY;
+
+            particleSystem.update(sx, sy, now);
+
+            final float xc = mXOrigin;
+            final float yc = mYOrigin;
+            final float xs = mMetersToPixelsX;
+            final float ys = mMetersToPixelsY;
+            final int count = particleSystem.getParticleCount();
+            for (int i = 0; i < count; i++) {
+                final float x = xc + particleSystem.getPosX(i) * xs;
+                final float y = yc - particleSystem.getPosY(i) * ys;
+                particleSystem.mBalls[i].setTranslationX(x);
+                particleSystem.mBalls[i].setTranslationY(y);
+            }
+            invalidate();
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
         }
     }
-
 }
