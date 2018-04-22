f74d9008c4313e52a0dda99520a5029d79d641bb
Alpay Sabuncuoglu <Alpay.Sabuncuoglu@n11.com>
2018-04-22 22:16:33 +0300

    Blockly Blocks Added

--- blockly/generators.js
+++ blockly/generators.js
@@ -1,10 +1,3 @@
-Blockly.JavaScript['set_block'] = function(block) {
-  var value_set_value = Blockly.JavaScript.valueToCode(block, 'set_value', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
-
 Blockly.JavaScript['pulley_block'] = function(block) {
   // TODO: Assemble JavaScript into code variable.
   var code = '...';
@@ -33,6 +26,34 @@ Blockly.JavaScript['set_experiment_block'] = function(block) {
 };
 
 Blockly.JavaScript['end_block'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...;\n';
+  return code;
+};
+
+Blockly.JavaScript['constantacceleration_blok'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['ohmslaw_block'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['lever_block'] = function(block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
+};
+
+Blockly.JavaScript['set_angle_block'] = function(block) {
+  var number_angle_value = block.getFieldValue('angle_value');
   // TODO: Assemble JavaScript into code variable.
   var code = '...;\n';
   return code;
